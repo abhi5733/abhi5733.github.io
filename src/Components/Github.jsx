25ac4bce1356d2776f8935ea284bef8139533d06
abhi5733 <abhijeetkale5733@gmail.com>
2023-01-12 02:33:25 +0530

    add

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -5,42 +5,40 @@ import { BsAwardFill , BsFillBootstrapFill   } from "react-icons/bs";
 
 const Github = () => {
   return (
-   <Box border="1px solid black" width="70%" margin="auto" > 
+   <Box border="1px solid black" width={["90%","80%","70%","70%"]} margin="auto" > 
      
-     <Heading mt={20} textAlign={"center"} >Github  Activity</Heading>
+     <Heading mt={[10,10,20,20]} textAlign={"center"} >Github  Activity</Heading>
 
 
-    <VStack  mt={10} > 
-    <GitHubCalendar  username="abhi5733" />
+    <VStack  mt={10} border="1px solid black" width="100%"  > 
+    <GitHubCalendar  width="100%"   username="abhi5733" />
     </VStack>
     
     <Flex justifyContent="space-between" >
-    <VStack mt={10} justifyContent="start" border="1px solid black" >
-        <Flex  border="1px solid black"  alignItems="center" ><IconButton icon={<BsAwardFill/>}  /><Text>1200+ Hours Practical Coding</Text></Flex>
-        <Flex border="1px solid black" alignItems="center" ><IconButton icon={<BsAwardFill/>}  /><Text>300+ Hours Data Structures & Algorithms
-
-
-
-</Text></Flex>
-        <Flex border="1px solid black"  alignItems="center" ><IconButton icon={<BsAwardFill/>}  /><Text>100+ Hours Soft Skill Development</Text></Flex>
+    <VStack  alignItems="flex-start"   mt={10} justifyContent="start" border="1px solid black" >
+        <Flex  border="1px solid black"  alignItems="center" ><IconButton size={["xs","sm","md","lg"]} icon={<BsAwardFill/>} mr={[1,1,5,5]}  /><Text fontSize={[8,12,14,"2xl"]} >1200+ Hours Practical Coding</Text></Flex>
+        <Flex border="1px solid black" alignItems="center" ><IconButton size={["xs","sm","md","lg"]} icon={<BsAwardFill/>} mr={[1,1,5,5]}  /><Text fontSize={[8,12,14,"2xl"]} >300+ Hours Data Structures & Algorithms</Text></Flex>
+        <Flex border="1px solid black"  alignItems="center" ><IconButton size={["xs","sm","md","lg"]} icon={<BsAwardFill/>} mr={[1,1,5,5]}  /><Text fontSize={[8,12,14,"2xl"]} >100+ Hours Soft Skill Development</Text></Flex>
     </VStack>
-        <VStack  mt={10} border="1px solid black" >
-        <Flex   border="1px solid black"  alignItems="center" alignContent="start"  ><IconButton icon={<BsAwardFill/>}  />  <Text>12+ Projects</Text></Flex>
-        <Flex border="1px solid black"  alignItems="center" ><IconButton icon={<BsAwardFill/>}  /><Text>70+ Mini Projects</Text></Flex>
-        <Flex border="1px solid black"  alignItems="center" ><IconButton icon={<BsAwardFill/>}  /><Text>136+ Git Commits</Text></Flex>
+        <VStack alignItems="flex-start"   mt={10} border="1px solid black" >
+        <Flex   border="1px solid black"  alignItems="center" alignContent="start"  ><IconButton size={["xs","sm","md","lg"]} icon={<BsAwardFill/>} mr={[1,1,5,5]}   />  <Text fontSize={[8,12,14,"2xl"]} >12+ Projects</Text></Flex>
+        <Flex border="1px solid black"  alignItems="center" ><IconButton size={["xs","sm","md","lg"]} icon={<BsAwardFill/>} mr={[1,1,5,5]}  /><Text  fontSize={[8,12,14,"2xl"]} >70+ Mini Projects</Text></Flex>
+        <Flex border="1px solid black"  alignItems="center" ><IconButton size={["xs","sm","md","lg"]} icon={<BsAwardFill/>} mr={[1,1,5,5]}   /><Text fontSize={[8,12,14,"2xl"]} >136+ Git Commits</Text></Flex>
         </VStack>
     </Flex>
 
-<Flex alignItems="center" justifyContent="space-between"  border="1px solid black" mt={20} >  
-    <Image color="blackAlpha.400" bgColor="blackAlpha.400" border="1px solid black" borderRadius={10} align="center" src="https://github-readme-stats.vercel.app/api?username=abhi5733&show_icons=true&locale=en" alt="abhi5733" /> 
+<Flex flexDirection= {["column" , "column" , "column" , "row" ]}  alignItems="center" justifyContent="space-between"  border="1px solid black" mt={[10,10,20,20]} >  
+   <VStack border="1px solid black"  > 
+    <Image width="80%" color="blackAlpha.400" bgColor="blackAlpha.400" border="1px solid black" borderRadius={10} align="center" src="https://github-readme-stats.vercel.app/api?username=abhi5733&show_icons=true&locale=en" alt="abhi5733" /> 
+    </VStack>
 
-
-
-<Image border="1px solid black" borderRadius={10} align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=abhi5733&" alt="abhi5733" /> 
+    <VStack border="1px solid black"> 
+<Image width="80%" mt={[5,5,5,0]} border="1px solid black" borderRadius={10} align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=abhi5733&" alt="abhi5733" /> 
+</VStack>
   </Flex>
 
-  <Flex mt={10} justifyContent="center" > 
-<Image   border="1px solid black" borderRadius={10} align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=abhi5733&show_icons=true&locale=en&layout=compact" alt="abhi5733" /> 
+  <Flex mt={[5,5,5,10]} justifyContent="center" > 
+<Image width={["80%","80%","75%","50%"]}  border="1px solid black" borderRadius={10} align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=abhi5733&show_icons=true&locale=en&layout=compact" alt="abhi5733" /> 
 
 
 
